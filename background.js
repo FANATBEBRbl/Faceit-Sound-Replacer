4070f88cc9f607e4726af90589220c4b99cb0b02
Nikita Leonov <fanatbebrbl@gmail.com>
2025-07-18 03:18:32 +0300

    micro fix

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    let customFile = "custom.mp3";
-    if (details.url.includes("found-tone-silly.mp3")) customFile = "custom1.mp3";
-    else if (details.url.includes("large-crowd.mp3")) customFile = "custom2.mp3";
-    else if (details.url.includes("lone-wolf-howling.mp3")) customFile = "custom3.mp3";
-    else if (details.url.includes("radio-impact-swirl.mp3")) customFile = "custom4.mp3";
-    else if (details.url.includes("thunder-clap.mp3")) customFile = "custom5.mp3";
-    else if (details.url.includes("whoosh-whip.mp3")) customFile = "custom6.mp3";
+    let customFile = "custom1.mp3";
+    if (details.url.includes("found-tone-silly.mp3")) customFile = "custom2.mp3";
+    else if (details.url.includes("large-crowd.mp3")) customFile = "custom3.mp3";
+    else if (details.url.includes("lone-wolf-howling.mp3")) customFile = "custom4.mp3";
+    else if (details.url.includes("radio-impact-swirl.mp3")) customFile = "custom5.mp3";
+    else if (details.url.includes("thunder-clap.mp3")) customFile = "custom6.mp3";
+    else if (details.url.includes("whoosh-whip.mp3")) customFile = "custom7.mp3";
     const customUrl = chrome.runtime.getURL("sounds/" + customFile);
     return { redirectUrl: customUrl };
   },
